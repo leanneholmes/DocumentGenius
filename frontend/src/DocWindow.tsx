a55d78d0ea224ca3caa6eafaf075bae59d87a14a
GyoungHwan-Hong <hongkh5218@gmail.com>
2023-05-16 17:22:03 -0700

    get href from anchor tag from the html doc.

--- frontend/src/DocWindow.tsx
+++ frontend/src/DocWindow.tsx
@@ -1,21 +1,51 @@
+import { useEffect } from 'react';
 import './App.css';
 
 export default function DocWindow(props: { html: string }) {
   if (props.html === '') {
-    return <div className="docWindows">
-      <h1>This AI doesn't make up answers.</h1>
+    return (
+      <div className="docWindows">
+        <h1>This AI doesn't make up answers.</h1>
 
-      <p>This is where we show you the documents you've uploaded that we base our answers on.</p>
-      
-      <p>Our goal is to make you more efficient without having to validate the answers you receive from the AI.</p>
-    
+        <p>
+          This is where we show you the documents you've uploaded that we base
+          our answers on.
+        </p>
 
-    </div>;
+        <p>
+          Our goal is to make you more efficient without having to validate the
+          answers you receive from the AI.
+        </p>
+      </div>
+    );
+  } else {
+    useEffect(() => {
+      const anchors = document.querySelectorAll('.AnsFromDocument a');
+      anchors.forEach((anchor) => {
+        console.log(anchor.getAttribute('href'));
+      });
+
+      return () => {
+        <div
+          className="AnsFromDocument"
+          dangerouslySetInnerHTML={{ __html: props.html }}
+        />;
+      };
+    });
   }
+
+  const handleAnchorClick = (event: MouseEvent) => {
+    event.preventDefault(); 
+
+    console.log('Not implemented yet');
+  };
 
   return (
     <>
-      <div className="AnsFromDocument" dangerouslySetInnerHTML={{ __html: props.html }} />
+      <div
+        className="AnsFromDocument"
+        dangerouslySetInnerHTML={{ __html: props.html }}
+      />
     </>
   );
 }
