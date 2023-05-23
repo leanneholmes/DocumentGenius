aa9eeb4449d808a9e1e0b52fd6d2c5e9326b9d5e
GyoungHwan-Hong <hongkh5218@gmail.com>
2023-05-22 19:29:27 -0700

    Fixed wrong reference.

--- extensions/chrome/popup.js
+++ extensions/chrome/popup.js
@@ -20,8 +20,8 @@ document.getElementById("message-form").addEventListener("submit", function(even
       localStorage.setItem("conversation_state", conversation_state);
     }
 
-    // send post request to server os.getenv("API_KEY") with message in json body
-    fetch( os.getenv("API_KEY") + '/api/answer', {
+    // send post request to server http://127.0.0.1:5000/ with message in json body
+    fetch( import.meta.env.VITE_API_HOST + '/api/answer', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
