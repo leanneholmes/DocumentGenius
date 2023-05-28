154741deaac91f23fa59d9afd183a946cda76e35
Paul Prescod <paul@prescod.net>
2023-05-28 13:41:20 -0700

    Minor changes from prod

--- application/start.sh
+++ application/start.sh
@@ -1,2 +1,2 @@
-celery -A app.celery worker -l INFO & 
-gunicorn -w 2 --timeout 120 --bind 0.0.0.0:5001 wsgi:app
+python -m celery -A app.celery worker -l INFO &
+python -m gunicorn -w 2 --timeout 120 --log-level=debug --bind 0.0.0.0:5001 wsgi:app
